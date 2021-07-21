1d86b142199f217b16d9ea5801e0f50485217c95
RopeJuve <rope.s@hotmail.com>
2021-07-21 22:48:39 +0200

    1va

--- src/User.jsx
+++ src/User.jsx
@@ -5,7 +5,9 @@ const User = ({ name, userName, email, phone }) => {
   return (
     <div className="Card">
       <img src="https://gomendracollege.edu.np/Images/Default/default.png" />
+
       <p>Name: {name}</p>
+
       <p>User Name: {userName}</p>
       <p>Email: {email}</p>
       <p>Phone: {phone}</p>
